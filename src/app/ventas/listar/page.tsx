4ead78989a76f0f957a9f073fd78570dc5cdec3e
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-01 19:54:28 -0500

    Agregar dependencias clsx y tailwind-merge, crear componente Button y actualizar página ListarVentas

---
+++ src/app/ventas/listar/page.tsx
@@ -0,0 +1,128 @@
+import Button from "@/components/button/Button";
+
+export default function ListarVentas() {
+  return (
+    <div className="flex flex-col items-center">
+      <p className="text-3xl">Ventas</p>
+      <div className="flex flex-col w-full py-4 items-center justify-center">
+        <div className="flex items-center">
+          <label htmlFor="txtCliente">Cliente:</label>
+          <input type="text" id="txtCliente" className="border h-[30px] ml-4" />
+
+          <div className="px-8 flex gap-x-2">
+            <Button title="Buscar" />
+            <Button title="Ver todos" />
+          </div>
+
+          <label htmlFor="txtIdVenta">Id Venta</label>
+          <input type="text" id="txtIdVenta" className="border h-[30px] ml-4" />
+        </div>
+      </div>
+      <div className="flex flex-col w-full p-4">
+        <div className="max-h-[150px] w-full overflow-y-scroll">
+          <table className="bg-slate-300 w-full">
+            <thead className="bg-slate-50">
+              <tr>
+                <th className="py-2">Id Venta</th>
+                <th>Cliente</th>
+                <th>Fecha</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">2</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+              <tr>
+                <td className="text-center py-2">1</td>
+                <td className="text-center">Juan Perez</td>
+                <td className="text-center">01/01/2023</td>
+                <td className="text-center">$100.00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="flex flex-col gap-y-4">
+          <p className="text-2xl">Materiales</p>
+          <div className="max-h-[150px] w-full overflow-y-scroll">
+            <table className="bg-slate-300 w-full">
+              <thead className="bg-slate-50">
+                <tr>
+                  <th className="py-2">Id Venta</th>
+                  <th>Material</th>
+                  <th>Cantidad</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="text-center py-2">1</td>
+                  <td className="text-center">Material 1</td>
+                  <td className="text-center">10</td>
+                  <td className="text-center">$50.00</td>
+                </tr>
+                <tr>
+                  <td className="text-center py-2">1</td>
+                  <td className="text-center">Material 2</td>
+                  <td className="text-center">5</td>
+                  <td className="text-center">$25.00</td>
+                </tr>
+                <tr>
+                  <td className="text-center py-2">1</td>
+                  <td className="text-center">Material 3</td>
+                  <td className="text-center">2</td>
+                  <td className="text-center">$10.00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,18 @@
+import cx from "@/libs/cx";
+
 interface ButtonProps {
   title: string;
+  className?: string;
 }
 
-export default function Button({ title }: ButtonProps) {
+export default function Button({ title, className }: ButtonProps) {
   return (
-    <button className="py-2 px-5 max-w-max bg-blue-400 text-white rounded-xl">
+    <button
+      className={cx(
+        "py-2 px-5 max-w-max bg-blue-400 text-white rounded-xl",
+        className
+      )}
+    >
       {title}
     </button>
   );
