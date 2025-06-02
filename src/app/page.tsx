8d85bf1e1fbc76d63c3c28a6e00cf9d65324d1a2
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-01 21:39:14 -0500

    Eliminar enlaces de navegación y simplificar el componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div className="flex p-2 gap-x-4">
-      <Link
-        href="/ventas"
-        className="py-2 px-5 max-w-max bg-blue-400 text-white rounded-xl"
-      >
-        Ventas
-      </Link>
-      <Link
-        href="/mantenimiento/preventivo"
-        className="py-2 px-5 max-w-max bg-blue-400 text-white rounded-xl"
-      >
-        Mantenimiento Preventivo
-      </Link>
-    </div>
-  );
+  return <div className="flex p-2 gap-x-4 w-full">Hola</div>;
 }
