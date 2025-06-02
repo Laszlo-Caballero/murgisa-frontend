5721c7b8c08470c4ed099330a42d1fe863eabb12
ManuL0129 <manumonla@hotmail.com>
2025-06-01 20:44:41 -0500

    hola

--- src/data/links.tsx
+++ src/data/links.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { AiFillHome } from "react-icons/ai";
 import { HiCash } from "react-icons/hi";
+import { GrHostMaintenance } from "react-icons/gr";
 export const links: {
   icono: ReactNode;
   href: string;
@@ -20,5 +21,15 @@ export const links: {
     icono: <HiCash />,
     href: "/ventas/listar",
     texto: "Ver Ventas",
+  },
+  {
+    icono: <GrHostMaintenance />,
+    href: "/mantenimiento/correctivo",
+    texto: "Ver Mantenimiento Correctivo",
+  },
+  {
+    icono: <GrHostMaintenance />,
+    href: "/mantenimiento/preventivo",
+    texto: "Ver Mantenimiento Preventivo",
   },
 ];
