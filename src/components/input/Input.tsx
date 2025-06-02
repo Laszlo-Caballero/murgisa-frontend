ddd8268d43a672ac2aa9e143ad450b0f8b6e28d7
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-01 20:51:25 -0500

    Agregar componente Input con soporte para etiquetas, tipos y clases personalizadas

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,3 +1,23 @@
-export default function Input() {
-  return <div></div>;
+import cx from "@/libs/cx";
+import { HTMLInputTypeAttribute } from "react";
+
+interface InputProps {
+  label: string;
+  type?: HTMLInputTypeAttribute;
+  id: string;
+  className?: string;
+}
+
+export default function Input({
+  label,
+  type = "text",
+  id,
+  className,
+}: InputProps) {
+  return (
+    <div className={cx("flex flex-col", className)}>
+      <label htmlFor={id}>{label}</label>
+      <input type={type} id={id} />
+    </div>
+  );
 }
