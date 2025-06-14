ea2777c597cb01f67b1cff01a0cd1fedd9da61c4
luciana-31 <lgogny74@gmail.com>
2025-06-14 01:10:28 -0500

    feat: agregar tabla de profesiones

--- src/app/(dashboard)/profesion/page.tsx
+++ src/app/(dashboard)/profesion/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { LuGraduationCap } from "react-icons/lu";
 import { FiPlus } from "react-icons/fi";
 import Button from "@/components/ui/button/Button";
@@ -6,6 +7,10 @@ import { LuBookOpen } from "react-icons/lu";
 import { LuCircleCheckBig } from "react-icons/lu";
 import { LuUsers } from "react-icons/lu";
 import { LuFilter } from "react-icons/lu";
+import Table from "@/components/ui/table/Table";
+import { profesionData } from "@/data/profesion";
+import { use } from "react";
+
 
 export default function page() {
   return (
@@ -41,26 +46,98 @@ export default function page() {
           icon={<LuCircleCheckBig size={28} className="text-white" />}
           description="4"
           extra="Disponibles para asignación"
-          className = {{ container: "bg-purple-100 shadow-lg", icon: "bg-purple-600 rounded-full p-3",text:{title:"text-purple-700" ,description:"text-purple-900 text-3xl" ,extra: "text-purple-600"}}}
+          className = {{ container: "bg-purple-100 shadow-md", icon: "bg-purple-600 rounded-full p-3",text:{title:"text-purple-700" ,description:"text-purple-900 text-3xl" ,extra: "text-purple-600"}}}
         />
         <Card
           title="Personal Asignado"
           icon={<LuUsers size={28} className="text-white" />}
           description="31"
           extra="Empleados con profesión"
-          className = {{ container: "bg-orange-100 shadow-lg", icon: "bg-orange-600 rounded-full p-3", text:{title:"text-orange-700" ,description:"text-orange-900 text-3xl" ,extra: "text-orange-600"} }}
+          className = {{ container: "bg-orange-100 shadow-md", icon: "bg-orange-600 rounded-full p-3", text:{title:"text-orange-700" ,description:"text-orange-900 text-3xl" ,extra: "text-orange-600"} }}
 
         />
       </div>
           <div className="py-4 flex w-full flex-col gap-y-4">
-            <section className="flex w-full flex-col p-4 rounded-lg shadow bg-white">
-              <span className="flex items-center gap-x-2 font-medium text-black text-2xl">
-                <LuFilter />
-                Filtros
+            <section className="flex w-full flex-col p-4 rounded-md shadow-md bg-white">
+              <span className="flex items-center gap-x-2 font-semibold text-black text-md">
+                <LuFilter size={20} className="text-blue-700"/>
+                Filtros de Búsqueda
               </span>
+              <p className="text-sm mt-1 text-gray-500">Utiliza los filtros para encontrar profesiones específicas de manera rápida</p>
             </section>
       </div>
 
+      <div className="flex flex-col items-start justify-between w-full rounded-md shadow-md p-4 bg-white">
+        <div className="flex items-center justify-between w-full">
+          <div>
+            <p className="font-medium text-black text-xl">Profesiones Registradas</p>
+            <p className="text-sm text-gray-500 mt-1">Gestiona las profesiones disponibles en el sistema</p>
+          </div>
+          <div>
+            <span className="bg-gray-50 text-xs text-gray-600 font-semibold border border-gray-300 rounded-full px-3 py-1"> Total: 5 </span>
+          </div>
+        </div>
+
+        { <Table className="mt-4 bg-white w-full rounded-md shadow-md" data={profesionData} columns={[
+          {
+            header: "Profesión",
+            cell: (props) => {
+              return (
+              <div className="flex items-start gap-x-3">
+                <span className="bg-blue-100 p-2 rounded-xl">
+                  <LuGraduationCap size={15} className="text-blue-600" />
+                </span>
+                <div className="flex flex-col">
+                  <p className="font-semibold text-sm">{props.row.titulo}</p>
+                  <p className="text-xs text-gray-600">ID: {props.row.idProfesion}</p>
+                </div>
+              </div>
+
+              );
+            },
+          },
+          {
+            header: "Descripción ",
+            cell: (props) => {
+              return (
+                <p className="text-xs text-gray-600">{props.row.descripcion}</p>
+              );
+            }
+          },
+          {
+            header: "Personal",
+            cell: (props) => {
+              return (
+                <div className="flex items-center gap-x-2">
+                  <LuUsers size={15} className="text-gray-500" />
+                  <span className="text-sm font-bold">{props.row.cantidad}</span>
+                  <p className="text-sm"> empleados</p>
+                </div>
+              );
+            }
+          },
+          {
+            header: "Estado",
+            cell: (props) => {
+              return (
+                <div className=" flex items-center gap-x-2 py-1 px-3 bg-green-100 rounded-full border border-green-300">
+                  <LuCircleCheckBig size={15} className="text-green-800" />
+                  <span className=" text-xs font-semibold text-green-800">
+                    Activo
+                  </span>
+                </div>
+              );
+            },
+          },
+          {
+            header: "Acciones",
+          }
+          
+          
+        ]}>
+          
+        </Table> }
+      </div>
     </div>
   );
 }
---
+++ src/data/profesion.tsx
@@ -0,0 +1,9 @@
+import { Profesion } from "@/interfaces/response.interface"
+
+export const profesionData: Profesion[] = [{
+    idProfesion: 1,
+    titulo: "Ingeniero Mecánico",
+    descripcion: "Profesional especializado en diseño y mantenimiento de sistemas mecánicos",
+    cantidad: 12,
+    estado: true
+}]
