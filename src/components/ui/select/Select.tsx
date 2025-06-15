77be17b3505dfdb770cbe862c0cb2212d26521a6
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-14 23:23:51 -0500

    feat: add Load component for loading state and Select component for dropdown selection

---
+++ src/components/ui/select/Select.tsx
@@ -0,0 +1,95 @@
+import cx from "@/libs/cx";
+import React, {
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+  useState,
+} from "react";
+type Options = {
+  value: string;
+  label: string | ReactNode;
+};
+
+interface SelectProps {
+  label?: string;
+  id?: string;
+  icon?: ReactNode;
+  className?: string;
+  options?: Options[];
+  onChange?: (value: string) => void;
+  value?: Options;
+  placeholder?: string;
+}
+export default function Select({
+  className,
+  icon,
+  id,
+  label,
+  onChange,
+  options,
+  value,
+  placeholder,
+}: SelectProps) {
+  const parseIcon = icon as ReactElement<{
+    size?: number;
+    color?: string;
+    className?: string;
+  }>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col relative gap-y-2">
+      <label
+        htmlFor={id}
+        className="text-sm font-medium"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {label}
+      </label>
+      <div
+        className="w-full relative"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {isValidElement(icon) &&
+          cloneElement(parseIcon, {
+            size: 20,
+            className: cx(
+              "absolute top-1/2 -translate-y-1/2 left-3 text-slate-500",
+              parseIcon.props.className
+            ),
+            color: "#94A3B8",
+          })}
+        <div
+          id={id}
+          className={cx(
+            "w-full px-10 py-2 border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
+            className
+          )}
+        >
+          {value?.label || placeholder}
+        </div>
+      </div>
+      {open && (
+        <div className="absolute top-full flex flex-col gap-y-2 left-0 translate-y-2 w-full bg-white border border-slate-300 rounded-md shadow-lg z-10">
+          {options?.map((option) => {
+            return (
+              <div
+                className="w-full px-10 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer hover:bg-gray-100"
+                key={option.value}
+                onClick={() => {
+                  onChange?.(option.value);
+                }}
+              >
+                {option.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/modules/personal/crear/CrearPersonal.tsx
+++ src/modules/personal/crear/CrearPersonal.tsx
@@ -1,13 +1,61 @@
+"use client";
 import Input from "@/components/ui/input/Input";
 import React from "react";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import { LuUserRound } from "react-icons/lu";
 import { TbCash } from "react-icons/tb";
 import { LuPhone } from "react-icons/lu";
+import Select from "@/components/ui/select/Select";
+import { LuHouse } from "react-icons/lu";
+import { MdAttachMoney } from "react-icons/md";
+import Button from "@/components/ui/button/Button";
+import { useQuery } from "@/hooks/useQuery";
+import axios from "axios";
+import { env } from "@/config/env";
+import {
+  Departamento,
+  Profesion,
+  ResponseCargo,
+} from "@/interfaces/response.interface";
+import Load from "@/components/ui/load/Load";
 
-export default function CrearPersonal() {
+interface CrearPersonalProps {
+  onClose?: () => void;
+  onSave?: () => void;
+}
+
+export default function CrearPersonal({ onClose, onSave }: CrearPersonalProps) {
+  const { data: profesiones, isLoading: loadingProfesiones } = useQuery<
+    Profesion[]
+  >({
+    queryFn: async () => {
+      const response = await axios.get(`${env.url_api}/profesion`);
+      return response.data;
+    },
+  });
+
+  const { data: cargos, isLoading: loadingCargos } = useQuery<ResponseCargo>({
+    queryFn: async () => {
+      const response = await axios.get(`${env.url_api}/cargo`);
+      return response.data;
+    },
+  });
+
+  const { data: departamentos, isLoading: loadingDepartamentos } = useQuery<
+    Departamento[]
+  >({
+    queryFn: async () => {
+      const response = await axios.get(`${env.url_api}/departamento`);
+      return response.data;
+    },
+  });
+
   return (
-    <div className="min-w-[800px] rounded-lg bg-white p-8 flex flex-col gap-y-4">
+    <form className="min-w-[800px] rounded-lg bg-white p-8 flex flex-col gap-y-4">
+      {loadingProfesiones ||
+        loadingCargos ||
+        (loadingDepartamentos && <Load />)}
+
       <header>
         <p className="text-xl font-semibold">Agregar Nuevo Empleado</p>
         <p className="text-sm text-gray-500">
@@ -38,7 +86,49 @@ export default function CrearPersonal() {
           placeholder="920121233"
         />
         <Input label="Telefono" icon={<LuPhone />} placeholder="999999999" />
+
+        <Select
+          icon={<LuUserRound />}
+          label="Cargo"
+          placeholder="Seleccione un cargo"
+          options={cargos?.cargos.map((cargo) => ({
+            value: cargo.idCargo.toString(),
+            label: cargo.cargo,
+          }))}
+        />
+
+        <Select
+          icon={<LuUserRound />}
+          label="Profesion"
+          placeholder="Seleccione una profesion"
+          options={profesiones?.map((profesion) => ({
+            value: profesion.idProfesion.toString(),
+            label: profesion.titulo,
+          }))}
+        />
+
+        <Select
+          icon={<LuHouse />}
+          label="Departamento"
+          placeholder="Seleccione un departamento"
+          options={departamentos?.map((departamento) => ({
+            value: departamento.idDepartamento.toString(),
+            label: departamento.titulo,
+          }))}
+        />
+
+        <Input
+          label="Salario"
+          icon={<MdAttachMoney />}
+          placeholder="999999999"
+        />
+
+        <div className="flex items-center gap-x-4 justify-end w-full col-span-2 scroll-py-20">
+          <Button className="bg-red-500 px-12">Cerrar</Button>
+
+          <Button className="px-12">Guardar</Button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
