11ed6c4aff2ebb4809f520cb70aa484362f6b1e0
luciana-31 <lgogny74@gmail.com>
2025-06-16 01:18:05 -0500

    Agregar modal para Proveedor y arreglar modales de recurso

--- src/app/(dashboard)/tipo-recurso/page.tsx
+++ src/app/(dashboard)/tipo-recurso/page.tsx
@@ -3,7 +3,9 @@ import Button from "@/components/ui/button/Button";
 import CardInfo from "@/components/ui/card-info/CardInfo";
 import Card from "@/components/ui/card/Card";
 import { tipoRecursoData } from "@/data/tipoRecurso";
-import { HiPlusSmall } from "react-icons/hi2";
+import CrearTipoRecurso from "@/modules/tipo-recurso/crear/CrearTipoRecurso";
+
+
 import { FiPlus } from "react-icons/fi";
 import { LuLayers } from "react-icons/lu";
 import { LuCircleCheckBig } from "react-icons/lu";
@@ -26,31 +28,7 @@ export default function TipoRecursoPage() {
             setShowModal(false);
           }}
         >
-          <div className="w-[800px] bg-white p-6 rounded-lg shadow-lg">
-            <header className="flex items-center gap-x-3">
-              <LuPackage size={24} className="text-red-600" />
-              <div className="flex flex-col">
-                <p className="text-xl font-semibold">Agregar Tipo de Recurso</p>
-                <p className="text-sm text-gray-500">
-                  Completa los datos para registrar un nuevo tipo de recurso en
-                  el sistema
-                </p>
-              </div>
-            </header>
-            <div className="grid grid-cols-1 gap-4">
-              <Input
-                label="Nombre del Tipo de Recurso"
-                icon={<LuPackage />}
-                placeholder="Ej: Recurso"
-              />
-            </div>
-            <div>
-              <Button className="flex items-center gap-x-3 mt-4 bg-red-600 text-white py-3 font-semibold hover:bg-blue-500">
-                <FiPlus size={15} className="mr-2" />
-                Registrar Tipo de Recurso
-              </Button>
-            </div>
-          </div>
+          <CrearTipoRecurso/>
         </Modal>
       )}
       <header className="flex items-center justify-between">
