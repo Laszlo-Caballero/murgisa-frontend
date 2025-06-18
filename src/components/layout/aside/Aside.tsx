69d92e70f1eff95d91970b15fc6a277d5f7bb063
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-18 00:07:16 -0500

    Restaurar el componente Aside en el layout principal y mejorar la lógica de apertura/cierre en función del tamaño de la ventana.

--- src/components/layout/aside/Aside.tsx
+++ src/components/layout/aside/Aside.tsx
@@ -1,7 +1,7 @@
 "use client";
 import cx from "@/libs/cx";
 import Link from "next/link";
-import { cloneElement, ReactElement, useState } from "react";
+import { cloneElement, ReactElement, useEffect, useState } from "react";
 import { links } from "@/data/links";
 import { LuBuilding2 } from "react-icons/lu";
 import { getColor } from "@/libs/getColor";
@@ -12,128 +12,175 @@ import { RiLogoutCircleRLine } from "react-icons/ri";
 export default function Aside() {
   const pathName = usePathname();
   const [open, setOpen] = useState(false);
+  const [openAside, setOpenAside] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 1280) {
+        setOpenAside(true);
+      } else {
+        setOpenAside(false);
+      }
+    });
+  }, []);
 
   return (
-    <aside
+    <div
       className={cx(
-        "h-screen flex flex-col items-center sticky top-0 transition-all w-80"
+        "xl:w-auto z-10 overflow-x-hidden xl:overflow-x-visible xl:top-0 h-screen",
+        openAside ? "absolute xl:sticky bg-black/10 w-full" : "fixed"
       )}
+      onClick={() => {
+        if (window.innerWidth < 1280) {
+          setOpenAside(false);
+        }
+      }}
     >
-      <header className="flex items-center p-6 w-full bg-blue-700 text-white gap-x-2">
-        <span className="bg-white size-10 rounded-lg flex items-center justify-center">
-          <LuBuilding2 className="text-blue-700" size={25} />
-        </span>
-        <div className="flex flex-col">
-          <p className="font-bold text-xl">MURGISA</p>
-          <p className="text-sm">Panel de Control</p>
-        </div>
-      </header>
+      <aside
+        className={cx(
+          "h-screen flex flex-col items-center z-10 bg-white top-0 transition-all",
+          openAside ? "w-80" : "w-0"
+        )}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <header className="flex items-center p-6 w-full bg-blue-700 text-white gap-x-2">
+          <span className="bg-white size-10 rounded-lg flex items-center justify-center">
+            <LuBuilding2 className="text-blue-700" size={25} />
+          </span>
+          <div className="flex flex-col">
+            <p className="font-bold text-xl">MURGISA</p>
+            <p className="text-sm">Panel de Control</p>
+          </div>
+          <button
+            className={cx(
+              "xl:hidden ml-auto top-4 left-4 z-20 bg-white p-2 rounded-full shadow-md",
+              !openAside && "fixed"
+            )}
+            onClick={() => setOpenAside(!openAside)}
+          >
+            <LuBuilding2
+              size={25}
+              className={cx(
+                "text-blue-700 transition-transform",
+                openAside ? "rotate-180" : ""
+              )}
+            />
+          </button>
+        </header>
 
-      <div className="px-4 pb-6 flex flex-col h-full w-full overflow-y-auto">
-        {links.map((linkGroup, index) => {
-          const color = getColor(linkGroup.color);
+        <div className="px-4 pb-6 flex flex-col h-full w-full overflow-y-auto">
+          {links.map((linkGroup, index) => {
+            const color = getColor(linkGroup.color);
 
-          return (
-            <div
-              className="flex flex-col w-full gap-y-3 border-b border-slate-500/30 py-4"
-              key={index}
-            >
-              <span
-                className={cx(
-                  "px-2.5 py-0.5 text-xs rounded-full max-w-max font-semibold",
-                  color.bgColor,
-                  color.textColor
-                )}
+            return (
+              <div
+                className="flex flex-col w-full gap-y-3 border-b border-slate-500/30 py-4"
+                key={index}
               >
-                {linkGroup.titulo}
-              </span>
-              <div className="flex flex-col">
-                {linkGroup.links.map((link, linkIndex) => {
-                  const paseIcon = link.icono as ReactElement<{
-                    className?: string;
-                    size?: number;
-                    color?: string;
-                  }>;
+                <span
+                  className={cx(
+                    "px-2.5 py-0.5 text-xs rounded-full max-w-max font-semibold",
+                    color.bgColor,
+                    color.textColor
+                  )}
+                >
+                  {linkGroup.titulo}
+                </span>
+                <div className="flex flex-col">
+                  {linkGroup.links.map((link, linkIndex) => {
+                    const paseIcon = link.icono as ReactElement<{
+                      className?: string;
+                      size?: number;
+                      color?: string;
+                    }>;
 
-                  return (
-                    <Link
-                      href={link.href}
-                      key={linkIndex}
-                      className={cx(
-                        "flex items-center rounded-sm px-2 py-4 transition-colors",
-                        pathName === link.href && color.activeColor
-                      )}
-                    >
-                      <div className="flex items-center gap-x-6 w-full">
-                        {cloneElement(paseIcon, {
-                          className: cx(
-                            pathName === link.href
-                              ? color.textColor
-                              : "text-gray-500"
-                          ),
-                          size: 15,
-                        })}
-                        <span
+                    return (
+                      <Link
+                        href={link.href}
+                        key={linkIndex}
+                        className={cx(
+                          "flex items-center rounded-sm px-2 py-4 transition-colors",
+                          pathName === link.href && color.activeColor
+                        )}
+                        onClick={() => {
+                          if (window.innerWidth < 1280) {
+                            setOpenAside(false);
+                          }
+                        }}
+                      >
+                        <div className="flex items-center gap-x-6 w-full">
+                          {cloneElement(paseIcon, {
+                            className: cx(
+                              pathName === link.href
+                                ? color.textColor
+                                : "text-gray-500"
+                            ),
+                            size: 15,
+                          })}
+                          <span
+                            className={cx(
+                              "text-sm font-medium",
+                              pathName === link.href
+                                ? color.textColor
+                                : "text-gray-500"
+                            )}
+                          >
+                            {link.label}
+                          </span>
+                        </div>
+                        <IoChevronForwardOutline
+                          size={15}
                           className={cx(
-                            "text-sm font-medium",
                             pathName === link.href
                               ? color.textColor
                               : "text-gray-500"
                           )}
-                        >
-                          {link.label}
-                        </span>
-                      </div>
-                      <IoChevronForwardOutline
-                        size={15}
-                        className={cx(
-                          pathName === link.href
-                            ? color.textColor
-                            : "text-gray-500"
-                        )}
-                      />
-                    </Link>
-                  );
-                })}
+                        />
+                      </Link>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-
-      <footer className="w-full p-4 bg-gray-50 relative">
-        <div className="flex items-center justify-between p-3 bg-white rounded-lg shadow-md">
-          <div className="flex items-center">
-            <span className="size-10 bg-blue-600 rounded-full text-white flex items-center justify-center">
-              A
-            </span>
-            <div className="ml-3">
-              <p className="text-sm font-semibold">Administrador</p>
-              <p className="text-xs text-gray-500">admin@gmail.com</p>
-            </div>
-          </div>
-
-          <button onClick={() => setOpen(!open)}>
-            <LuSettings
-              size={20}
-              className="text-gray-600 hover:text-blue-600 transition-colors cursor-pointer"
-              aria-label="Configuración"
-            />
-          </button>
+            );
+          })}
         </div>
 
-        {open && (
-          <div className="absolute bottom-1/2 translate-y-1/2 left-full w-full translate-x-5 right-0 bg-white shadow-2xl">
-            <Link
-              href="/login"
-              className="p-4 flex items-center gap-x-2 cursor-pointer"
-            >
-              <RiLogoutCircleRLine />
-              Logout
-            </Link>
+        <footer className="w-full p-4 bg-gray-50 relative">
+          <div className="flex items-center justify-between p-3 bg-white rounded-lg shadow-md">
+            <div className="flex items-center">
+              <span className="size-10 bg-blue-600 rounded-full text-white flex items-center justify-center">
+                A
+              </span>
+              <div className="ml-3">
+                <p className="text-sm font-semibold">Administrador</p>
+                <p className="text-xs text-gray-500">admin@gmail.com</p>
+              </div>
+            </div>
+
+            <button onClick={() => setOpen(!open)}>
+              <LuSettings
+                size={20}
+                className="text-gray-600 hover:text-blue-600 transition-colors cursor-pointer"
+                aria-label="Configuración"
+              />
+            </button>
           </div>
-        )}
-      </footer>
-    </aside>
+
+          {open && (
+            <div className="absolute bottom-1/2 translate-y-1/2 left-full w-full translate-x-5 right-0 bg-white shadow-2xl">
+              <Link
+                href="/login"
+                className="p-4 flex items-center gap-x-2 cursor-pointer"
+              >
+                <RiLogoutCircleRLine />
+                Logout
+              </Link>
+            </div>
+          )}
+        </footer>
+      </aside>
+    </div>
   );
 }
