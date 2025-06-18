52d5a1a9a07aae94d3a3820867f8d38aa9445032
laszlo-caballero <laszlocaballero@gmail.com>
2025-06-17 23:42:01 -0500

    Agregar atributos de accesibilidad al componente Modal

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -10,6 +10,9 @@ export default function Modal({ children, onClose }: ModalProps) {
     <div
       className="w-full h-screen fixed top-0 left-0 bg-black/50 z-50 flex items-center justify-center"
       onClick={onClose}
+      role="dialog"
+      aria-modal="true"
+      tabIndex={-1}
     >
       <div
         onClick={(e) => {
