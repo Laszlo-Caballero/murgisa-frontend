f2ee001cf00273811cdbafa2e47d66683c8263ff
AntonellaHonores <antonellahonores15@gmail.com>
2025-06-20 00:29:39 -0500

    modal oscuro

--- src/modules/cargo/crear/CrearCargo.tsx
+++ src/modules/cargo/crear/CrearCargo.tsx
@@ -7,17 +7,17 @@ import { FiPlus } from "react-icons/fi";
 
 export default function CrearCargo() {
   return (
-    <div className="w-full max-w-sm md:max-w-3xl bg-white p-6 rounded-xl shadow-lg flex flex-col gap-6">
+    <div className="w-full max-w-sm md:max-w-3xl bg-white p-6 rounded-xl shadow-lg flex flex-col gap-6 dark:bg-gray-800 border dark:border-gray-700">
       <header className="flex items-center gap-x-3">
         <LuBriefcase size={40} className="text-blue-600" />
         <div className="flex flex-col">
-          <p className="text-xl font-semibold">Agregar Nuevo Cargo</p>
+          <p className="text-xl font-semibold  dark:text-white">Agregar Nuevo Cargo</p>
           <p className="text-sm text-gray-500">
             Completa los datos para registrar un nuevo cargo en el sistema
           </p>
         </div>
       </header>
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4  dark:text-white">
         <Input
           label="Nombre del Cargo"
           icon={<BsFilePost />}
@@ -31,7 +31,7 @@ export default function CrearCargo() {
       </div>
       <div>
         <Button className="flex items-center gap-x-3 mt-4 bg-blue-600 text-white py-3 font-semibold hover:bg-blue-500">
-          <FiPlus size={15} className="mr-2" />
+          <FiPlus size={15} className="mr-2 " />
           Registrar Cargo
         </Button>
       </div>
