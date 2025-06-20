387b3fbf0b95eaed5a5dc0ef7b89a342aebe0a62
AntonellaHonores <antonellahonores15@gmail.com>
2025-06-20 01:31:59 -0500

    modal tipo de recurso

--- src/modules/tipo-recurso/crear/CrearTipoRecurso.tsx
+++ src/modules/tipo-recurso/crear/CrearTipoRecurso.tsx
@@ -8,18 +8,18 @@ import { FiPlus } from "react-icons/fi";
 
 export default function CrearTipoRecurso() {
   return (
-    <div className="w-full max-w-sm md:max-w-3xl rounded-lg bg-white p-8 flex flex-col gap-y-4">
+    <div className="w-full max-w-sm md:max-w-3xl rounded-lg bg-white p-8 flex flex-col gap-y-4  dark:bg-gray-800 border dark:border-gray-700">
       <header className="flex items-center gap-x-3">
         <LuLayers size={40} className="text-red-600 mb-2" />
         <div className="flex flex-col">
-          <p className="text-xl font-semibold">Agregar Nuevo Tipo de Recurso</p>
+          <p className="text-xl font-semibold  dark:text-white">Agregar Nuevo Tipo de Recurso</p>
           <p className="text-sm text-gray-500">
             Completa los datos para registrar un nuevo tipo de recurso en el
             sistema
           </p>
         </div>
       </header>
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4  dark:text-white">
         <Input
           label="Nombre del Tipo de Recurso"
           icon={<LuLayers />}
