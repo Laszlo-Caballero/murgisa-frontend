2ace4d9ef95d00f2c2466768be9ed98bbef58bd2
luciana-31 <lgogny74@gmail.com>
2025-07-11 16:02:39 -0500

    feat: Implementar botones de editar y desactivar en las tarjetas de Cargo, Departamento, Forma de Pago, Personal y Tipo de Mantenimiento

--- src/cards/PersonalCard.tsx
+++ src/cards/PersonalCard.tsx
@@ -3,6 +3,13 @@ import CardInfo from "@/components/ui/card-info/CardInfo";
 import { Personal } from "@/interfaces/responsefinal.interface";
 import { LuBriefcase, LuCalendar, LuPhone } from "react-icons/lu";
 import { MdMoneyOff, MdNumbers, MdOutlineEmail } from "react-icons/md";
+import Button from "@/components/ui/button/Button";
+import DeleteModal from "@/components/share/delete-modal/DeleteModal";
+
+import {
+    LuSquarePen,
+    LuTrash2,
+} from "react-icons/lu";
 
 export const PersonalCard = (empleado: Personal) => {
   return (
@@ -17,7 +24,9 @@ export const PersonalCard = (empleado: Personal) => {
           icon: "bg-blue-100 text-center p-0 size-7 flex items-center justify-center uppercase dark:bg-blue-500/30",
           description: "dark:text-gray-400",
         },
-        span: "bg-green-100 text-green-600 font-bold dark:bg-green-500/30 dark:text-green-300 dark:border-green-700",
+        span: empleado?.estado
+          ? "bg-green-100 text-green-600 dark:bg-green-500/30 dark:text-green-300 dark:border-green-700"
+          : "bg-red-100 text-red-600 dark:bg-red-500/30 dark:text-red-300 dark:border-red-700",
       }}
       description={empleado?.cargo?.cargo}
       span={empleado?.estado ? "Activo" : "Inactivo"}
@@ -60,7 +69,19 @@ export const PersonalCard = (empleado: Personal) => {
             </p>
           </span>
         </div>
-      </div>
+        <div className="flex items-center justify-between gap-x-2">
+            <Button className="flex items-center gap-x-3 py-1 font-semibold mt-2 bg-white dark:bg-red-500/10 text-red-500 border border-red-300 dark:border-red-400 hover:bg-red-50 dark:hover:bg-red-600/20">
+            <LuSquarePen size={15} />
+            Editar
+            </Button>
+            <div className="flex items-center gap-x-3 py-1 px-3 font-semibold mt-2 bg-white dark:bg-gray-700 text-black dark:text-white border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-600 rounded-sm text-sm">
+              <DeleteModal id={empleado?.idPersonal} endpoint="personal">
+              <LuTrash2 className="text-gray-900 dark:text-gray-400 cursor-pointer" />
+              </DeleteModal>
+              Desactivar
+            </div>
+          </div>
+        </div>
     </CardInfo>
   );
 };
