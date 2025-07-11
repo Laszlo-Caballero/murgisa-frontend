57ca4e81f169add2bc4f4948f5d92bd0c21acb2d
AntonellaHonores <antonellahonores15@gmail.com>
2025-07-11 18:35:44 -0500

    arreglo modal

--- src/modules/preventivo/Crear/CrearPreventivo.tsx
+++ src/modules/preventivo/Crear/CrearPreventivo.tsx
@@ -151,159 +151,112 @@ export default function CrearPreventivo({ onClose }: ModalProps) {
       onSubmit={handleSubmit(mutate)}
       className="w-full max-w-sm md:max-w-3xl bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg"
     >
+      <header className="flex items-center gap-x-3">
+        <LuShield size={40} className="text-orange-600" />
+        <div className="flex flex-col">
+          <p className="text-xl font-semibold dark:text-white">
+            Agregar Mantenimiento Preventivo
+          </p>
+          <p className="text-sm text-gray-500">
+            Completa los datos para registrar un nuevo mantenimiento preventivo
+            en el sistema
+          </p>
+        </div>
+      </header>
         {(isLoading || loadingMantenimientoPreventivo) && <Load />}
-     <div className="w-full max-w-sm md:max-w-3xl bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
-               <header className="flex items-center gap-x-3">
-                 <GrHostMaintenance size={40} className="text-orange-500" />
-                 <div className="flex flex-col">
-                   <p className="text-xl font-semibold dark:text-white">
-                     Agregar una planificación de mantenimiento
-                   </p>
-                   <p className="text-sm text-gray-500 dark:text-gray-300">
-                     Completa los datos para registrar una nueva planificación en
-                     el sistema
-                   </p>
-                 </div>
-               </header>
-   
-            
-               <div className="grid grid-cols-1 gap-4 mt-4">
-                 <div className="flex flex-col gap-y-1">             
-                   <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                        <InputDate
-                          label="Fecha de Mantenimiento"
-                          icon={<CiCirclePlus />}
-                          placeholder="Selecciona una fecha"
-                          error={errors.fechaMantenimiento?.message}
-                          onChange={(date) => {
-                              setValue("fechaMantenimiento", date);
-                          }}
-                          value={watch("fechaMantenimiento")}
-                          />
-                     </div>
-                  </div>
-                 <div className="flex flex-col gap-y-1">
-                   <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                   <Select
-                      label="Prioridad"
-                      icon={<MdLowPriority />}
-                      placeholder="Selecciona una Prioridad"
-                      options={[
-                        { label: "Alta", value: "1" },
-                        { label: "Media", value: "2" },
-                        { label: "Baja", value: "3" },
-                      ]}
-                      onChange={(value) => {
-                        setValue("Prioridad", value.value);
-                      }}
-                      value={{ label: getPrioridadLabel(watch("Prioridad")), value: watch("Prioridad") }}
-                      error={errors.Prioridad?.message}
-                    />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4 dark:text-gray-300">
+                <InputDate
+                  label="Fecha de Mantenimiento"
+                  icon={<CiCirclePlus />}
+                  placeholder="Selecciona una fecha"
+                  error={errors.fechaMantenimiento?.message}
+                  onChange={(date) => setValue("fechaMantenimiento", date)}
+                  value={watch("fechaMantenimiento")}
+                />
 
-                   </div>
-                 </div>
-                 <div className="flex flex-col gap-y-1">
-                     <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                     <Select
-                              label="Recurso"
-                              icon={<CiCirclePlus />}
-                              placeholder="Selecciona un Recurso"
-                              options={recurso?.data?.map((recurso) => {
-                                return {
-                                  label: recurso.nombre,
-                                  value: recurso.idRecurso.toString(),
-                                };
-                              })}
-                              onChange={(value) => {
-                                setValue("recurso", {
-                                  value: value.value,
-                                  label: value.label?.toString() || "",
-                                });
-                              }}
-                              value={watch("recurso")}
-                              error={errors.recurso?.message}
-                      />
-                   </div>
-                 </div>
-  
-                 <div className="flex flex-col gap-y-1">
-                   <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                     <Select
-                              label="Personal"
-                              icon={<CiCirclePlus />}
-                              placeholder="Selecciona un Personal"
-                               options={personal?.data?.map((personal) => {
-                                return {
-                                  label: personal.nombre,
-                                  value: personal.idPersonal.toString(),
-                                };
-                              })}
-                              onChange={(value) => {
-                                setValue("personal", {
-                                  value: value.value,
-                                  label: value.label?.toString() || "",
-                                });
-                              }}
-                              value={watch("personal")}
-                              error={errors.personal?.message}
-                      />
-                   </div>
-                 </div>
-   
-                 <div className="flex flex-col gap-y-1">
-                    <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                      <Select
-                              label="Horario"
-                              icon={<CiCirclePlus />}
-                              placeholder="Selecciona un Horario"
-                              options={data?.data?.map((horario) => {
-                                return {
-                                  label:`${horario.horaInicio.split("T")[1].substring(0, 5)} - ${horario.horaFin.split("T")[1].substring(0, 5)}`,
-                                  value: horario.idhorario.toString(),
-                                };
-                              })}
-                              onChange={(value) => {
-                                setValue("horario", {
-                                  value: value.value,
-                                  label: value.label?.toString() || "",
-                                });
-                              }}
-                              value={watch("horario")}
-                              error={errors.horario?.message}
+                <Select
+                  label="Prioridad"
+                  icon={<MdLowPriority />}
+                  placeholder="Selecciona una Prioridad"
+                  options={[
+                    { label: "Alta", value: "1" },
+                    { label: "Media", value: "2" },
+                    { label: "Baja", value: "3" },
+                  ]}
+                  onChange={(value) => setValue("Prioridad", value.value)}
+                  value={{ label: getPrioridadLabel(watch("Prioridad")), value: watch("Prioridad") }}
+                  error={errors.Prioridad?.message}
+                />
 
-                      />
-                   </div>
-                   </div>
-                    <div className="flex flex-col gap-y-1">
-                    <div className="grid lg:grid-cols-2 gap-4 dark:text-gray-300">
-                      <Select
-                                label="Tipo de Mantenimiento"
-                                icon={<PiWrenchBold />}
-                                placeholder="Tipo de Mantenimiento Correctivo"
-                                options={tipos?.data.map((tipo) => {
-                                  return {
-                                    label: tipo.nombre,
-                                    value: tipo.idTipoMantenimiento.toString(),
-                                  };
-                                })}
-                                onChange={(value) => {
-                                  setValue("tipo", {
-                                    value: value.value,
-                                    label: value.label?.toString() || "",
-                                  });
-                                }}
-                                value={watch("tipo")}
-                                error={errors.tipo?.message}
-                              ></Select>
-                   </div>
-                 </div>
-               </div>
-   
+                <Select
+                  label="Recurso"
+                  icon={<CiCirclePlus />}
+                  placeholder="Selecciona un Recurso"
+                  options={recurso?.data?.map((r) => ({
+                    label: r.nombre,
+                    value: r.idRecurso.toString(),
+                  }))}
+                  onChange={(value) => setValue("recurso", {
+                    value: value.value,
+                    label: value.label?.toString() || "",
+                  })}
+                  value={watch("recurso")}
+                  error={errors.recurso?.message}
+                />
+
+                <Select
+                  label="Personal"
+                  icon={<CiCirclePlus />}
+                  placeholder="Selecciona un Personal"
+                  options={personal?.data?.map((p) => ({
+                    label: p.nombre,
+                    value: p.idPersonal.toString(),
+                  }))}
+                  onChange={(value) => setValue("personal", {
+                    value: value.value,
+                    label: value.label?.toString() || "",
+                  })}
+                  value={watch("personal")}
+                  error={errors.personal?.message}
+                />
+
+                <Select
+                  label="Horario"
+                  icon={<CiCirclePlus />}
+                  placeholder="Selecciona un Horario"
+                  options={data?.data?.map((horario) => ({
+                    label: `${horario.horaInicio.split("T")[1].substring(0, 5)} - ${horario.horaFin.split("T")[1].substring(0, 5)}`,
+                    value: horario.idhorario.toString(),
+                  }))}
+                  onChange={(value) => setValue("horario", {
+                    value: value.value,
+                    label: value.label?.toString() || "",
+                  })}
+                  value={watch("horario")}
+                  error={errors.horario?.message}
+                />
+
+                <Select
+                  label="Tipo de Mantenimiento"
+                  icon={<PiWrenchBold />}
+                  placeholder="Tipo de Mantenimiento Correctivo"
+                  options={tipos?.data.map((tipo) => ({
+                    label: tipo.nombre,
+                    value: tipo.idTipoMantenimiento.toString(),
+                  }))}
+                  onChange={(value) => setValue("tipo", {
+                    value: value.value,
+                    label: value.label?.toString() || "",
+                  })}
+                  value={watch("tipo")}
+                  error={errors.tipo?.message}
+                />
+              </div>
                 <Button className="flex items-center gap-x-3 mt-4 bg-orange-500 text-white py-3 font-semibold hover:bg-orange-400">
                  <FiPlus size={15} className="mr-2" />
                  Registrar Planificación
                </Button>
-             </div>
+        
               </form>
   );
 }
