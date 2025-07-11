459316019f41c35d428bfe9738ad4d0d208d9864
laszlo-caballero <laszlocaballero@gmail.com>
2025-07-11 18:36:28 -0500

    fix: Cambiar método de actualización de departamento de PATCH a PUT

--- src/modules/departamento/actualizar/ActualizarDepartamento.tsx
+++ src/modules/departamento/actualizar/ActualizarDepartamento.tsx
@@ -45,7 +45,7 @@ export default function ActualizarDepartamento({
     Response<Departamento[]>
   >({
     mutationFn: async (data, url, token) => {
-      const response = await axios.patch(
+      const response = await axios.put(
         `${url}/departamento/${id}`,
         {
           titulo: data.titulo,
